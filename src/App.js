b068030ef6107f51502a0ecb9baf8e145dd622b0
Muhammad Hashim <muhammmadhashimsardar@gamil.com>
2023-07-02 10:14:20 +0500

    justupdating

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Bookdata from './books.json'
 
 function App() {
+  
+    
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        
+        {
+          Bookdata.map((person) =>(
+            <li key={person.id}>
+              {person.title} - {person.genre}
+            </li>
+          ))
+        }
+      
+
+      </div>
     </div>
   );
 }
