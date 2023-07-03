9b1427bc78cd263552fa10dc2beda12cb99cbd57
Muhammad Hashim <muhammmadhashimsardar@gamil.com>
2023-07-03 10:17:23 +0500

    Done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,109 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
-import Bookdata from './books.json'
+import axios from 'axios';
+
 
 function App() {
-  
+  const [data, setdata] = useState([]);
+  const [value, setvalue] = useState("")
+  const [vatlesort,setsortvalue ]=useState([])
+
+  const sortoption = ["Microsoft .NET", "Java", "BusinessClient-Server", "object-Oriented Programming"]
+
+  useEffect(() => {
+    const getdata = async () => {
+      return await axios.get("http://localhost:5000/posts").then((response) => setdata(response.data));
+    }
+
+    getdata()
+
+  }, [])
+
+  // const searchPosts = (value) => {
+  //   value.preventDefault();
+  //   const filteredPosts = data.filter((post) => {
+  //     return post.title.toLowerCase().includes(value.toLowerCase());
+  //   });
+  //   setdata(filteredPosts);
+  // };
+ 
+  const handlesearch = (e) => {
     
+    const filteredData = data.filter((item) => item.title.toLowerCase().includes(value.toLowerCase()));
+    setdata(filteredData);
+  }; 
+  console.log(data)
+  // const handlesearch =async(e)=> {
+  //   setIsButtonDisabled(!isButtonDisabled);
+  //   e.preventDafult();
+  //   setdata(Object.entries(data).find(element => element > value))
 
+  //   return await axios.get(`http://localhost:5000/posts?q=${value}`)
+  //   .then((response) => {
+  //   setdata(response.data)
+  //   setvalue("") 
+  // }
+  //  );
+  // }
 
-  return (
-    <div className="App">
+  const handlesort = async(e)=>{
+    let value=e.target.value
+    setsortvalue(value) 
+    return await axios.get(`http://localhost:5000/posts?_sort=${value}&_order=asc`)
+    .then((response) => {
+    setdata(response.data)
+    
+    // const filteredData = data.filter((item) => item.categories.toLowerCase().includes(vatlesort.toLowerCase()));
+    // setdata(filteredData);
+  })}
+
+   return (
+    <div className="App"> 
+     <form  className="form"action="submit ">
       <div>
-        
-        {
-          Bookdata.map((person) =>(
-            <li key={person.id}>
-              {<img src={person.thumbnailUrl} alt="ok" ></img>}  {person.title} - {person.genre} 
-            </li>
-          ))
-        }
-
+         <input type="text" className='form_input ' placeholder='Search Name .........' value={value} onChange={(e) => setvalue(e.target.value)}/>
+         <button type='submit '  onClick={() => handlesearch()}>Search</button>
       </div>
+      <div>
+        <select
+        onChange={handlesort} 
+             value={vatlesort}
+        >
+           <option >
+            please select at least one
+           </option>
+           {
+               sortoption.map((value, index) =>
+                  <option value={value} key={index}>
+                       {value}
+                  </option>
+               )
+           }
+        </select>
+      </div>
+     </form>
+      
+
+
+
+    <div>
+      {
+        data.map((person) => (
+          <div className="Books" key={person._id}>
+            <div className="cover">
+              <img src={person.thumbnailUrl} alt="books" />
+            </div>
+            <div>
+              <h5>{person.title}</h5>
+              <p className="author"><b>author:</b>{person.authors}</p>
+              <p><b>categories:</b>{person.categories}</p>
+            </div>
+          </div>
+        ))
+      }
+
     </div>
+    </div >
   );
 }
 
